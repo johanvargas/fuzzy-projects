a08933f56716672cbd0cb94ecbf05403cb3dc45c
Johan Vargas <dqbp00@gmail.com>
2020-06-22 02:27:23 -0400

    testing page

--- testing.py
+++ testing.py
@@ -1,7 +1,14 @@
 from pop import convert, get_num, table
 import random
 
+container = []
+container2 = []
 r = random.randint(0, 1000)
-print('{} is out number\n'.format(r))
-check = convert(get_num(r), 0)
-print(check)
+print(r)
+g = get_num(r)
+
+convert(g, 0)
+
+print(g)
+
+print(container)
