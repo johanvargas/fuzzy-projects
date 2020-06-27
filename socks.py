ebdb725574a39d8290d4a262a23cc825d3c2f32c
Johan Vargas <dqbp00@gmail.com>
2020-06-27 00:06:39 -0400

    some more scritps

---
+++ socks.py
@@ -0,0 +1,11 @@
+import socket
+
+socket.setdefaulttimeout(2)
+
+s = socket.socket()
+
+try:
+	s.connect(("127.0.0.1", 8000))
+	ans = s.recv(1024)
+except Exception, e:
+	print('Error {}'.format(e))
