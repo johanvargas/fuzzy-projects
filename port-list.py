aff01208757c9a9a130ee9d9335ab4e0a6ae9a12
johanvargas <johan.vargas@me.com>
2020-08-17 21:30:40 -0400

    update, better functionality

--- port-list.py
+++ port-list.py
@@ -3,68 +3,66 @@ import requests
 import random
 import time
 from bs4 import BeautifulSoup
-# import re
 
 """
+
 PORTS of SAN IPANEMA
+v 1.0
+By Johan Alberto Vargas
 
-Things to add
+Things To Add:
 
-Session view
-	start a game session
-	track right and wrong answers.
-	points system
-	reward system??
+	Session view
+		start a game session
+		track right and wrong answers.
+		points system
+		reward system??
 
-Game platform that compiles a system of learning for user.
+	Game platform that compiles a system of learning for user.
 
-User selects a topic.
-	db created of 
-		factoids
-		flash cards
-		quizes
-		vid references
-		pdf referecnces
-		imgs
-		resources 
+	User selects a topic.
+		db created of 
+			factoids
+			flash cards
+			quizes
+			vid references
+			pdf referecnces
+			imgs
+			resources 
 
-It could learn your learning habits and feed info accordingly
+Ideas:
 
-Not in use as of now, 08132020
-
-def selector(num):
-	next = int(num)
-	print(f'The number you selected is : {num}\n')
-	ans = next
-	return ans
-
-Main Game
-
-def find_the_pair():
-	# 0, 1 initial pairing from list, list_it
-	# then, even number is port number, p
-	# odd port name, p + 1
+	It could learn your learning habits and feed info accordingly.
 
 """
 
 
 def game_type():
+	# The start of the game. Introduces rules and sets up params for play
+
 	print('You have two options:')
 	print('You can select and Infinity Game - Where it continuosly answer Questions until you quit (Enter 1)')
 	print('Or you can select the number of Questions you want in a Session (Enter 2)')
-	selekt = int(input('What is you decision? ')) # This needs some formatting to make sure the types are consistant.
+	selekt = int(input('What is your decision? '))	
+	
+	# Inputs need some formatting to make sure the types are consistent.
+	# <selekt> selects number of games, either infinte or numbered.
+	# then prompts for selection of game type - number, name, mixed quest.
+	
 	print(f"Great, you selected {selekt}\n")
 	print('Now what type of game do you want to play?')
-	print('You have three options')
+	print('You have three options\n')
 	print('Option 1 - A Number Quest, where you answer the name of the corresponding port.')
 	print('Option 2 - A Name Quest, where you answer with the port number to the corresponding name.')
 	print('Option 3 - A Mixed Quest, where either a Name or a Number Quest is thrown at your weak little mind')
-	quest_type = int(input('So, what say you? enter the number of your fate... '))
+	print('\n')
+
+	quest_type = int(input('So, what say you? enter the number of your fate... \n'))
 
 	if selekt == 1:
-		sel1 = game_select(1, quest_type)
+		sel1 = game_select(selekt, quest_type)	# Infinite Games
 	elif selekt == 2:
-		sel2 = game_select(2, quest_type)
+		sel2 = game_select(selekt, quest_type)	# Numbered Games
 	else:
 		print('Naughty, not an option! Starting over...')
 		return game_type()
@@ -80,13 +78,13 @@ def game_select(game_num, quest_type):
 
 def game_20(quest_type):
 	question_num = input('How many questions do you want to play? ')
-	print(f"Great,{question_num} questions it is!")
+	print(f'Great,{question_num} questions it is!\n')
 	the_game(question_num, quest_type)
 
 
 def game_inf(quest_type):
 	print('Ok, lets get started! Good Luck!')
-	the_game(0, quest_type)
+	the_game(False, quest_type)
 
 
 def the_game(number_of_questions, quest_type):
@@ -103,84 +101,85 @@ def the_game(number_of_questions, quest_type):
 	list_it = list(spoonful)
 	length = len(list_it)
 
-	print(number_of_questions, quest_type)
-
 	def name_quest(number_of_questions, cool):
-		if cool == 'y':
-			def make_odd():
-				question_selector = random.randrange(0, length)
-				if question_selector % 2 == 0:
-					return make_odd()
-				else:
-					return int(question_selector)
+		def make_odd():
+			question_selector = random.randrange(0, length)
+			if question_selector % 2 == 0:
+				return make_odd()
+			else:
+				return int(question_selector)
 
-			index = make_odd()
-
-			fact = list_it[index].text
-			fact_answer = list_it[index - 1].text
-			answer = input(f'what is the port number for the port named {fact}? ')
-			print(f'the answer is {fact_answer}')
-			check = input('play again? (y/n)')
-			return name_quest(number_of_questions, check)
+		if number_of_questions == False:
+			if cool == 'y':
+				index = make_odd()
+				fact = list_it[index].text
+				fact_answer = list_it[index - 1].text
+				answer = input(f'what is the port number for the port named {fact}? ')
+				print(f'the answer is {fact_answer}')
+				check = input('play again? (y/n)')
+				return name_quest(number_of_questions, check)
+			else:
+				pass
 		else:
-			return
+			num = int(number_of_questions)
+			print(f'number_of_questions is {number_of_questions}')
+			
+			while num >= 0:
+				index = make_odd()
+				question_selector = random.randrange(0, length, 2)
+				fact = list_it[index].text
+				fact_answer = list_it[index - 1].text
+				answer = input(f'what is the port name for port number {fact} ')
+				print(f'the answer is {fact_answer}')
+				num-=1
+			else:
+				pass
+
+		print('End of Name Quest')
 
 	def number_quest(number_of_questions, cool):
-		if cool == 'y':
-			question_selector = random.randrange(0, length, 2)
-			fact = list_it[question_selector].text
-			fact_answer = list_it[question_selector + 1].text
-			answer = input(f'what is the port name for port number {fact} ')
-			print(f'the answer is {fact_answer}')
-			check = input('play again? (y/n)')
-			return number_quest(number_of_questions, check)
+		if number_of_questions == False:
+			if cool == 'y':
+				question_selector = random.randrange(0, length, 2)
+				fact = list_it[question_selector].text
+				fact_answer = list_it[question_selector + 1].text
+				answer = input(f'what is the port name for port number {fact} ')
+				print(f'the answer is {fact_answer}')
+				check = input('play again? (y/n)')
+				return number_quest(number_of_questions, check)
+			else:
+				pass 
 		else:
-			return
+			num = int(number_of_questions)
+			print(f'number_of_questions is {number_of_questions}')
+			while num >= 0:
+				question_selector = random.randrange(0, length, 2)
+				fact = list_it[question_selector].text
+				fact_answer = list_it[question_selector + 1].text
+				answer = input(f'what is the port name for port number {fact} ')
+				print(f'the answer is {fact_answer}')
+				num-=1
+			else:
+				pass
 
+		print('End of Number Quest')
 
-	def mixed_quest(number_of_questions):
-		pass
+	def mixed_quest(number_of_questions, cool):
+		if cool == 'y':
+			rand_quest_selector = random.randrange(0, 2)
+			print('rand_quest_selector')
 
 	if quest_type == 1:
 		number_quest(number_of_questions, 'y')
 	elif quest_type == 2:
 		name_quest(number_of_questions, 'y')
 	elif quest_type == 3:
-		mixed_quest()
+		mixed_quest(number_of_questions, 'y')
 
 def main():
 	print('Welcome... You have docked at the PORT OF SAN IPANEMA!\n')
 	print('Select a Game Type:\n\n')
 	game_type()
-
-# 	if True:
-
-# 		print('Ok.. lass uns spielen..')
-# 		# time.sleep(2)
-# 		print('The game consists of answering the correct name or port based on the info given\n')
-# 		print('If you don\'t know or want to move to the next question, type idk')
-# 		q_port = list_it[r]
-# 		print(f'The port is : {q_port.text}')
-
-# 		question = input('What is the corresponding service associated with this port? \n')
-# 		answer = question
-
-# 		if answer == list_it[r + 1].text:
-# 			print('Sehr gut!')
-# 			return find_the_pair()
-# 		elif answer == 'idk':
-# 			return find_the_pair()
-# 		else:
-# 			print('Tut mir leid, das is nicht die richtige Antwort!\n')
-# 			print(f'{list_it[r + 1].text.upper()} ist das Antwort.\n\n')
-
-# 		prompter = input('willst du nochmal spielen? \nType ja to play again\nOr press ENTER to exit\n')
-# 		if prompter == 'ja':
-# 			return find_the_pair()
-# 		else:
-# 			pass
-
-# 		pass
 
 if __name__ == '__main__':
 	main()
