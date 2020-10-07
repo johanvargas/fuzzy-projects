5ce029a6de5e5fa95219fa29b43b4d1f89848262
johanvargas <johan.vargas@me.com>
2020-10-07 17:30:45 -0400

    oops

--- pytest/mypillow.py
+++ pytest/mypillow.py
@@ -1,5 +1,6 @@
 import collections
 from PIL import Image
+from support import check_image_dir, get_images
 
 '''
 
@@ -7,42 +8,45 @@ Brute-forcey way of checking if two image histograms are equal,
 that is, if they are the same image. 
 
 '''
+# setup content
 
-list_test = [4, 6 ,7, 4,'12', 5, 4]
-ans = []
+check_image_dir()
 
-def compare_items(l):
-	# if collections.Counter(docket) == collections.Counter(image):
-	while len(l) > 0:
-		for n in range(1, len(l)):
-			print(l, l[n])
-			if l[0] == l[n]:
-				print(l[0], l[n])
-				ans.append(f'Found an equal at {l[0]}')
-			else:
-				pass
-		l.remove(l[0])
-		print('\n')
-		return compare_items(l)
-	return ans
+# list_test = [4, 6 ,7, 4,'12', 5, 4]
+# ans = []
 
-
-
-def compare(*args):
-	l = []
-	for e in args:
-		img_location = f'images/{e}.jpg'
-		open_img = Image.open(img_location)
-		histo = open_img.histogram()
-		l.append(histo)
-
-	return l
+# def compare_items(l):
+# 	# if collections.Counter(docket) == collections.Counter(image):
+# 	while len(l) > 0:
+# 		for n in range(1, len(l)):
+# 			print(l, l[n])
+# 			if l[0] == l[n]:
+# 				print(l[0], l[n])
+# 				ans.append(f'Found an equal at {l[0]}')
+# 			else:
+# 				pass
+# 		l.remove(l[0])
+# 		print('\n')
+# 		return compare_items(l)
+# 	return ans
 
 
-_set = compare(image1, image2, image3)
 
-comp = compare_items(list_test)
-print(comp)
+# def compare(*args):
+# 	l = []
+# 	for e in args:
+# 		img_location = f'images/{e}.jpg'
+# 		open_img = Image.open(img_location)
+# 		histo = open_img.histogram()
+# 		l.append(histo)
+
+# 	return l
+
+
+# _set = compare(image1, image2, image3)
+
+# comp = compare_items(list_test)
+# print(comp)
 # print(_set)
 
 
