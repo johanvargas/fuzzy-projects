4ab7fc0c3c5af58c6cdd19207abb8a908db3f999
johanvargas <johan.vargas@me.com>
2020-10-29 15:56:13 -0400

    0.02v roman numeral converter, up to 1,000,000. does have a big, check notes or readme

--- r_num_v2.py
+++ r_num_v2.py
@@ -1,6 +1,13 @@
 '''
-___WORKING NOTES
-Working up to 9999, unformated, ugly. 10292020
+
+Roman Numeral Converter v 0.02
+by Johan Vargas
+
+___WORKING NOTES_____
+
+Working up to 1,000,000, produces string of int passed. 
+
+BUG - Assigns multiplier (MILIMULTIPLIER) to repeats in the int.
 ________
 
 I = 1
@@ -60,40 +67,59 @@ get_int_len -> get_key
 null = 'nulla'
 
 MASTER_KEY = (
-	['M', '_V', '_X'], 
-	['C', 'D', 'M'], 
-	['X', 'L', 'C'], 
+	['M', 'V*', 'X*'],
+	['C', 'D', 'M'],	
+	['X', 'L', 'C'],
 	['I' , 'V', 'X']
 	)
 
-MILLI_MULTIPLIER = '_'
+MILLI_MULTIPLIER = ('_')
 
 # Main Functions
 
+# length of stringified number.
 def get_int_len(num):
 	return len(str(num))
 
+
+# Parses number to string and returns the roman numeral unformated.
 def int_to_key(num):
 	int_str = str(num)
 	container = ''
 
 	for i in range(0, get_int_len(num)):
 		if i + 1 == len(int_str):
-			print(int_str[i], i, num, MASTER_KEY[-1])
-			key_parser(int_str[i], MASTER_KEY[-1])
+			# print(int_str[i], i, num, MASTER_KEY[-1])
+			container+=key_parser(int_str[i], MASTER_KEY[-1], int_str)
 		elif i + 2 == len(int_str):
-			print(int_str[i], i, num, MASTER_KEY[-2])
-			key_parser(int_str[i], MASTER_KEY[-2])
+			# print(int_str[i], i, num, MASTER_KEY[-2])
+			container+=key_parser(int_str[i], MASTER_KEY[-2], int_str)
 		elif i + 3 == len(int_str):
-			print(int_str[i], i, num, MASTER_KEY[-3])
-			key_parser(int_str[i], MASTER_KEY[-3])
+			# print(int_str[i], i, num, MASTER_KEY[-3])
+			container+=key_parser(int_str[i], MASTER_KEY[-3], int_str)
 		elif i + 4 == len(int_str):
-			print(int_str[i], i, num, MASTER_KEY[-4])
-			key_parser(int_str[i], MASTER_KEY[-4])
+			# print(int_str[i], i, num, MASTER_KEY[-4])
+			container+=key_parser(int_str[i], MASTER_KEY[-4], int_str)
+		elif i + 5 == len(int_str):
+			# print(int_str[i], i, num, MASTER_KEY[-2])
+			container+=key_parser(int_str[i], MASTER_KEY[-2], int_str)
+		elif i + 6 == len(int_str):
+			# print(int_str[i], i, num, MASTER_KEY[-3])
+			container+=key_parser(int_str[i], MASTER_KEY[-3], int_str)
+		elif i + 7 == len(int_str):
+			# print(int_str[i], i, num, MASTER_KEY[-4])
+			container+=key_parser(int_str[i], MASTER_KEY[-4], int_str)
+		elif i + 8 == len(int_str):
+			# print(int_str[i], i, num, MASTER_KEY[-3])
+			container+=key_parser(int_str[i], MASTER_KEY[-3], int_str)
 
 	return container
 
-def key_parser(digit, key):
+# Recieves a number and returns the roman numeral equvalent of that digit based on its position in the number.
+def key_parser(digit, key, num):
+	int_length = get_int_len(num)
+	if digit == '0' and int_length == 1:
+		return null
 	if digit == '0':
 		return ''
 	table = [
@@ -108,8 +134,19 @@ def key_parser(digit, key):
 		(key[0] + key[2]),
 		key[2]
 	]
-	print(table[int(digit) - 1])
-	return table[int(digit) - 1]
+	# print('number length is ', int_length)
+	# print(num.index(digit))
+	if int_length > 4:		
+		if num.find(digit) == 0:
+			print(num, digit)
+			# print(MILLI_MULTIPLIER + str(table[int(digit) - 1]))
+			return MILLI_MULTIPLIER + str(table[int(digit) - 1])
+		else:
+			# print(table[int(digit) - 1])
+			return table[int(digit) - 1]
+	else:
+		# print(table[int(digit) - 1])
+		return table[int(digit) - 1]
 
 
 # TESTING FUNCTIONS
@@ -121,7 +158,10 @@ def test_number_range(r):
 
 # Test a single instance, a single integer
 def test_single_int(num):
-	print(int_to_key(num))
+	print(num, int_to_key(num))
 
 # TESTING
-test_single_int(69)
+test_single_int(66606)
+# test_number_range(101)
+
+	
