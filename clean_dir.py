ec8c40d32382ff18380167c34910956f81c6e268
johanvargas <johan.vargas@me.com>
2020-10-29 18:14:55 -0400

    some cleanup

--- clean_dir.py
+++ clean_dir.py
@@ -2,49 +2,52 @@ import sys
 import os
 import math
 
-## SYS MODULE
+# System and OS modules. Some basic methods.
+
+# SYS MODULE
 
 # size of storge drive??
 s = sys.maxsize
-#print(s)
+# print(s)
 
 # sys path
 p = sys.path
-#print(p)
+# print(p)
 
 # sys version
 v = sys.version
-#print(v)
+# print(v)
 
-## OS MODULE
+# OS MODULE
 
 # name of os
 o = os.name
-#print(o)
+# print(o)
 
 # lists files in dir
 d = os.listdir('.')
-#print(d)
+# print(d)
 
-# gets the times??
+# gets the time??
 t = os.times()
-print(t)
+# print(t)
 
 # generator function that walks the dir tree.
 w = os.walk('.')
 
 # look thru the dir and list all the files
 # Incomplete.. completing in .pytest/ 
-for root, dirs, files in os.walk('.'):
+for root, dirs, files in w:
     # print("root-->{}\ndirs-->{}\nfiles-->{}".format(root, dirs, files))
-    # print(f'{files}')
+    # print('{}'.format(files))
     for f in files:
         a = f.split('.')
-        print(type(f))
+        # print(type(f))
+        print(f)
 
 ## MATH MODULE
 
 # log(x, 10)
 # this will be very useful in roman numeral.py
 lg = math.log10(1000)
-print(lg)
+# print(lg)
--- pytest/mypillow.py
+++ pytest/mypillow.py
@@ -7,12 +7,12 @@ from support import check_image_dir, get_images, CWD, CURRENT_DIR, IMG_DIR_NAME
 Brute-forcey way of checking if two image histograms are equal, 
 that is, if they are the same image. 
 
+check_image_dir --> checks is images/ is present, if not it creates the dir and populates with a
+couple of images for testing. The rest is histo...grams.
+
 '''
 # setup support content
 check_image_dir()
-
-
-
 
 def get_histograms():
 	os.chdir(IMG_DIR_NAME)
@@ -42,68 +42,8 @@ def compare_histograms(img_list):
 		img_list.remove(img_list[0])
 		return compare_histograms(img_list)
 
-
-
-
 # TESTS
 
 histograms = get_histograms()
 compare = compare_histograms(histograms)
 print(compare)
-
-'''
-
-References and extra crap
-^^^^^^^^^^^^^^^^^^^^^^^^
-
-opens image in preview
-im1.show()
-im2.show()
-
-returns a list of values, length = 758 
-histo1 = im1.histogram()
-histo2 = im2.histogram()
-
-returning NoneType
-histo1 = im1.getcolors()
-histo2 = im2.getcolors()
-
-(r, b, g) dump
-s = im2.getdata()
-
-
-
-def open_image(image):
-	return Image.open(image)
-
-image.histogram()
-
-	# while len(img_list) > 0:f
-	# 	for image in img_list:
-	# 		print(image[0])
-	# 		for i in range(len(img_list) - 1):
-	# 			print (image[0], image[i][0])
-	# if collections.Counter(image_1) == collections.Counter(image_2):
-	# 	t = tuple(image[], image[])
-	# 	print('ok, we are in the money')
-	# else:
-	# 	print('they are NOT the same')
-
-# list_test = [4, 6 ,7, 4,'12', 5, 4]
-# ans = []
-
-# def compare_items(l):
-# 	while len(l) > 0:
-# 		for n in range(1, len(l)):
-# 			print(l, l[n])
-# 			if l[0] == l[n]:
-# 				print(l[0], l[n])
-# 				ans.append(f'Found an equal at {l[0]}')
-# 			else:
-# 				pass
-# 		l.remove(l[0])
-# 		print('\n')
-# 		return compare_items(l)
-# 	return ans
-
-'''
