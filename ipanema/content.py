29eca4c894a2d8e818da2d6222438ffeca6b9f73
Johan Vargas <dqbp00@gmail.com>
2021-03-16 00:40:36 -0400

    putting the feelers back out

--- ipanema/content.py
+++ ipanema/content.py
@@ -1,24 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 import random
+import sys
 
 def game_content():
-	# Content for game
-	# Needs a network connection error msg
-	page = requests.get('https://www.webopedia.com/quick_ref/portnumbers.asp')
+    # Content for game
+    # Needs a network connection error msg
+    # the webopedia changed their website, so now this link doesn't work
 
-	# Parse html
-	soup = BeautifulSoup(page.content, 'html.parser')
-	spoonful = soup.find_all('td')
-	list_it = list(spoonful)
-	length = len(list_it)
+    page = requests.get('https://www.webopedia.com/quick_ref/portnumbers.asp')
+    if page.status_code != '200':
+        print("error: exited with status : ", page.status_code)
+        sys.exit('We are done here, sorry')
+    # Parse html
+    soup = BeautifulSoup(page.content, 'html.parser')
+    spoonful = soup.find_all('td')
+    list_it = list(spoonful)
+    length = len(list_it)
 
-	r = random.randrange(0, length)
+    r = random.randrange(0, length)
 
-	# Use modulus to get the right order of question/answer pair.
-	# Which sounds like it should get parsed in a dict or obj
-	if r % 2 == 0 :
-		# print(list_it[r].text, list_it[r+1].text)
-		return [list_it[r].text, list_it[r+1].text]
-	else:
-		return game_content()
+    # Use modulus to get the right order of question/answer pair.
+    # Which sounds like it should get parsed in a dict or obj
+    if r % 2 == 0 :
+            # print(list_it[r].text, list_it[r+1].text)
+            return [list_it[r].text, list_it[r+1].text]
+    else:
+            return game_content()
