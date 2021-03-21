dc2daec0efc99dbe637eceddf613acd8c07eecc8
Johan Vargas <dqbp00@gmail.com>
2021-03-21 08:14:38 -0400

    update for pillow

--- requests-easy.py
+++ requests-easy.py
@@ -2,23 +2,28 @@ import requests
 from requests.auth import HTTPBasicAuth
 from requests.adapters import HTTPAdapter
 from requests.exceptions import ConnectionError
-from getpass import getpass
+from getpass import getpass, getuser
 # random note - f strings are from python 3.6+
+
+## At this point it does
+# it prints the headers and the full text of the reponse, the html etc...
 
 #r = requests.get('http://www.instagram.com/', auth=HTTPBasicAuth('getmegyoza', getpass()))
 
 session_adapter = HTTPAdapter(max_retries=1)
 
 savoy_url = 'http://www.savoyparkr.com/'
-insta_url = 'http://www.instagram.com/'
+insta_url = 'http://www.instagram.com/accounts/login'
+
+print(getuser())
 
 with requests.Session() as session:
-    session.auth = ('getmegyoza', getpass())
-    session.mount(savoy_url, session_adapter)
+#    session.auth = ('getmegyoza', getpass(prompt="getm*** password: ")) 
+#    session.mount(savoy_url, session_adapter)   # here to try and mitigate 429 response
     
     try:
         s = session.get(insta_url)
-        print(s.headers)
+#        print(s.headers)
         print(s.text)
     except ConnectionError as ce:
         print(ce)
